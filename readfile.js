690dad912f4c3b79f75b83151fde27edc4ecfea7
Rahulkakkireni <rahulkakkireni@gmail.com>
2024-01-31 20:48:18 +0530

    first-commit

---
+++ readfile.js
@@ -0,0 +1,7 @@
+4.	Write a NodeJS program to read from a file and display the content on screen – “readfile.js”
+var fs = require('fs'); try {
+var data = fs.readFileSync('my-file.txt', 'utf8'); console.log(data);
+} catch(e) { console.log('Error:', e.stack);
+}
+
+my-file.txt
